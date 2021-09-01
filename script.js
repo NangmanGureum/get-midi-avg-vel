b1501bf2b1728a0d9031367e569d9c49d97a06db
NangmanGureum <lastkwak@gmail.com>
2021-09-02 08:53:23 +0900

    Add Web MIDI Support

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let tb_note_num = document.querySelector("#tb-note-num");
 let tb_vel = document.querySelector("#tb-vel");
 let send_btn = document.querySelector(".send-button");
 let not_support = document.querySelector(".not-support");
+let div_midi_config = document.querySelector(".midi-config__content");
+let select_device_select = document.querySelector("#device-select");
+
 
 
 const IS_CHOROME = navigator.userAgent.indexOf("Chrome") !== -1;
@@ -12,11 +15,12 @@ let vel_list = [];
 // https://deeplify.dev/front-end/js/min-max-avg-in-array
 const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
 
+function paintMIDIOption(value, text) {
+    let option_object = document.createElement("option");
+    option_object.value = value;
+    option_object.textContent = text;
 
-function checkMidiAvailable() {
-    if (IS_CHOROME) {
-        not_support.classList.add("unsee");
-    }
+    select_device_select.add(option_object);
 }
 
 function paintMidiLog(note, vel) {
@@ -51,9 +55,50 @@ function sendMidi() {
     paintMidiLog(note, vel);
 }
 
+// Thanks for:
+// https://wookim789.tistory.com/28
+function onMIDIFailure() {
+    div_midi_config.classList.add("unsee");
+}
+
+function onMIDISuccess(midiAccess) {
+    not_support.classList.add("unsee");
+    console.log(midiAccess);
+    console.log(midiAccess.inputs.values());
+    const inputs = midiAccess.inputs;
+    for (let input of inputs.values()) {
+        // Device Status
+        console.log("====================================");
+        console.log("ID: " + input.id);
+        console.log("Manufacturer: " + input.manufacturer);
+        console.log("Name: " + input.name);
+        console.log("Type: " + input.type);
+        console.log("Version: " + input.version);
+        console.log("State: " + input.state);
+        console.log("Connection: " + input.connection);
+
+        // Paint Option
+        let option_text = input.name + "(" + input.manufacturer + ")";
+        paintMIDIOption(input.id, option_text)
+
+        input.onmidimessage = getMIDIMsg;
+      }
+}
+
+function getMIDIMsg(event) {
+    const data = event.data;
+    console.log(data);
+    // If note on
+    if (data[0] == 144) {
+        paintMidiLog(data[1], data[2])
+    }
+}
+
 function main() {
     send_btn.addEventListener("click", sendMidi);
-    checkMidiAvailable();
+    // Thanks for:
+    // https://wookim789.tistory.com/28
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 }
 
 main()
